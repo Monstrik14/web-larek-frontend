3285d777d8a774708b03c713f7d9bba3380616d1
Sergei Suprun <sergeysuprun@list.ru>
2025-01-21 13:36:20 +0300

    21.01

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
 import './scss/styles.scss';
+
+class ProductView {
+  constructor () {
+  }
+
+  set title(value:string) {
+    this.setText(this.container, value)
+  }
+}
+
+// api
+ const product = new ProductItem(добавить такой класс)
+Api. метод отображения товара (){
+  .then(res => {
+    product.(метод)(res)
+  })
+  .catch (err = > {
+    console.error(err)
+  })
+}
+
+// функция осздания карточки 
+
+function createCard(template: HTMLTemplateElement, name: string) {
+  const cardElement = template.content.querySelector('.card-preview').cloneNode(true) as HTMLElement;
+  return cardElement 
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,36 @@
 import { EventEmitter } from "../components/base/events";
+import { Component } from "../components/base/components";
+
+type price = number | null
+type payment = 'cash' | 'online'
+
+interface IProduct {
+  id: string;
+  title: string;
+  category: string;
+  description: string;
+  price: price;
+  image: string;
+}
+
+interface IOrder{
+  payment: payment;
+  mail: string;
+  phone: string;
+  address: string;
+  total: number;
+  items: string[]
+}
+
+interface IBasket {
+  items: string[];
+  total: price;
+}
+
+interface IOrderResult {
+  id: string;
+  total: price;
+}
 
 // interface корзины скнопками + и -
 interface IBasketModel{
@@ -7,17 +39,6 @@ interface IBasketModel{
   remove(id:string):void
 }
 
-interface ILotItem {
-  
-}
-interface IProduct {
-  id: string;
-  title: string;
-  category: string;
-  description: string;
-  price: Map<number, string> | null;
-  image: string;
-}
 
 // пример отображения товара в корзине
 interface IBasketGoods {
@@ -30,24 +51,15 @@ interface Payment<T> {
   items: IProduct[]
   payOnline: (obj: T) => T
   payCash: (obj: T) => T
+  address: string
 }
 
 // отображение элемента разметки
 
-interface Constructor {
-  new (container: HTMLElement, events?: EventEmitter): IView;
+interface IProductConstructor {
+  new (): Component<IProduct>;
 }
 
 interface IView {
   render(data?: object): HTMLElement;
-}
-
-// подгружаемые данные через api
- 
-
-// фунеция осздания карточки 
-
-function createCard(template: HTMLTemplateElement, name: string) {
-  const cardElement = template.content.querySelector('.card-preview').cloneNode(true) as HTMLElement;
-  return cardElement 
 }

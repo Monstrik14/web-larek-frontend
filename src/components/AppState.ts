9dbd86ced279d6a7bb50e3038e979284398aab2b
Sergei Suprun <sergeysuprun@list.ru>
2025-02-02 14:31:15 +0300

    Create AppState.ts

---
+++ src/components/AppState.ts
@@ -0,0 +1,115 @@
+import { IProduct, IOrder, IUserInfo } from "../types"
+import { IEvents } from "./base/events";
+
+export class AppState {
+protected  _productList: IProduct[] = [];
+protected _cartItems: IProduct[] = []; 
+protected _userData: Partial<IOrder> = { 
+  payment: 'cash', 
+  email: '', 
+  phone: '', 
+  address: '' 
+};
+protected _validationErrors: Partial<Record<keyof IOrder, string>>;
+protected currentpreview: string = '';
+
+constructor(protected events: IEvents) {
+}
+
+set productList(items: Partial<IProduct[]>) {
+  this._productList = items;
+  this.events.emit('items:update', items); 
+}
+
+set userData (data: Partial<IOrder>){
+  this.userData = data
+}
+
+
+get productList() { return this._productList; }
+get userData() { return this._userData; }
+get	cartItems() { return this._cartItems }
+
+get totalCartPrice(): number | null { 
+    return this.cartItems.reduce((acc, item) => acc + item.price, 0)
+} 
+
+
+get countBasket() { return this.cartItems.length }
+get formErrors() { return this._validationErrors }
+
+setPreviewItem(card: IProduct) {
+  this.currentpreview = card.id;
+  this.events.emit('preview:change', card);
+}
+
+getCartItemIds() {
+  return this.cartItems.map(item => item.id);
+  }
+
+getUser(): Omit<IOrder, 'total' | 'items'> {
+  return {
+    email: this._userData.email,
+    phone: this._userData.phone,
+    address: this._userData.address,
+    payment: this._userData.payment }
+}
+findProductById(id: string) {
+  return this.productList.find(item => item.id === id);
+}
+
+
+addToCart(id: string) {
+  this._cartItems.push(this._productList.find(item => item.id === id));
+  this.events.emit('basket:change')
+} 
+removeFromCart(id: string) {
+  this._cartItems = this.cartItems.filter(item => item.id !== id);
+  this.events.emit('basket:change'); 
+}
+
+clearCart() {
+  this._cartItems = [];
+  this.events.emit('basket:change');
+}
+
+validateUserData(): boolean {
+    const errors: typeof this._validationErrors = {};
+    if (!this.userData.address) {
+     errors.address = 'Необходимо указать адрес';
+    }
+    if (!this.userData.payment) {
+     errors.payment = 'Необходимо указать способ оплаты';
+    }
+    if (!this.userData.email) {
+     errors.email = 'Необходимо указать email';
+    }
+    if (!this.userData.phone) {
+     errors.phone = 'Необходимо указать номер телефона';
+    }
+    this._validationErrors = errors;
+    this.events.emit('input:error', this.formErrors);
+    return Object.keys(errors).length === 0;
+   }
+
+resetOrder() {
+  this._userData = { payment: 'cash', email: '', phone: '', address: '', total: 0, items: [] };
+  this.events.emit('inpit:error')
+} 
+
+updateOrderField(field: keyof IUserInfo, value: string & 'cash' | 'card') {
+  this._userData[field] = value;
+  this.validateUserData()
+}
+
+isProductInCart(id: string): boolean {
+  return this.cartItems.some((item) => item.id === id);
+}
+
+getField() {
+  return this.userData.payment;
+}
+}
+
+
+
